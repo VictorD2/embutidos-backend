01e2c007d03c82b2690bbedc659f43ef5e8601c2
Victor Hernandez <victor-2027@hotmail.com>
2022-08-23 15:58:30 -0500

    chore:

--- src/app.ts
+++ src/app.ts
@@ -60,7 +60,7 @@ class App {
   // Settings
   async settings() {
     dotenv.config();
-    this.app.set('port', this.port || process.env.PORT || 4000);
+    this.app.set('port', process.env.PORT);
     await ClsRol.createInitialRols();
     await ClsUser.creatingAdminUser();
   }
