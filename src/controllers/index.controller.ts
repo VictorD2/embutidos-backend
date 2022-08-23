aef4d29427a0447b7f2e360bd683aff9478d937f
Victor Hernandez <victor-2027@hotmail.com>
2022-08-23 15:35:27 -0500

    fix: :bug: add try catch in index routes

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,8 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import path from 'path';
+import boom from '@hapi/boom';
 
-const indexController = async (req: Request, res: Response) => {
-  return res.sendFile(path.join(__dirname, '../build/public', 'index.html'));
+const indexController = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    return res.sendFile(path.join(__dirname, '../build/public', 'index.html'));
+  } catch (error: any) {
+    return next(boom.internal(error.message));
+  }
 };
 export default indexController;
